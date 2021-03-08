4dbd0d18e07274bc20b67434e6029a417a7da089
YYR <315629555@qq.com>
2021-03-08 20:14:19 +0800

    save

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,74 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(llvm_asm)]
+#![feature(global_asm)]
+
+mod sbi;
+
+use core::panic::PanicInfo;
+use core::fmt::{self, Write};
+use crate::sbi::shutdown;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// const SYSCALL_EXIT: usize = 93;
+//
+// fn syscall(id: usize, args: [usize; 3]) -> isize {
+//     let mut ret: isize;
+//     unsafe {
+//         llvm_asm!("ecall"
+//             : "={x10}" (ret)
+//             : "{x10}" (args[0]), "{x11}" (args[1]), "{x12}" (args[2]), "{x17}" (id)
+//             : "memory"
+//             : "volatile"
+//         );
+//     }
+//     ret
+// }
+//
+// const SYSCALL_WRITE: usize = 64;
+//
+// pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+//     syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+// }
+//
+// pub fn sys_exit(xstate: i32) -> isize {
+//     syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+// }
+//
+// struct Stdout;
+//
+// impl Write for Stdout {
+//     fn write_str(&mut self, s: &str) -> fmt::Result {
+//         sys_write(1, s.as_bytes());
+//         Ok(())
+//     }
+// }
+//
+// macro_rules! print {
+//     ($fmt: literal $(, $($arg: tt)+)?) => {
+//         $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+//     }
+// }
+//
+// #[macro_export]
+// macro_rules! println {
+//     ($fmt: literal $(, $($arg: tt)+)?) => {
+//         print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+//     }
+// }
+//
+// pub fn print(args: fmt::Arguments) {
+//     Stdout.write_fmt(args).unwrap();
+// }
+
+
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() {
+    shutdown();
 }
