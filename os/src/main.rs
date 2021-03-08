392351f75cf6463384333221d30ac5ad69db3a33
YYR <315629555@qq.com>
2021-03-08 21:16:12 +0800

    feat: shutdown 功能实现

--- os/src/main.rs
+++ os/src/main.rs
@@ -68,7 +68,18 @@ fn panic(_info: &PanicInfo) -> ! {
 
 global_asm!(include_str!("entry.asm"));
 
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
+
 #[no_mangle]
 pub fn rust_main() {
+    clear_bss();
     shutdown();
 }
